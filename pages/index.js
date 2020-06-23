30b2b2db6392174226c779a87dbef469e590a383
mareestephan <stephanmaree@gmail.com>
2020-06-23 12:36:57 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Categories from '../components/categories'
+import Layout from '../components/layout'
+
+export default function Index(props) {
+  return (
+    <>
+
+      <Layout>
+        <div>
+
+          <Categories categories={props.data} />
+
+        </div>
+      </Layout>
+
+    </>
+  )
+}
+
+export async function getServerSideProps() {
+
+  // const res = await fetch('https://my-json-server.typicode.com/mareestephan/jsonProducts/pages_to_create');
+  // const res = await fetch('http://www.json-generator.com/api/json/get/ckeElWByRe?indent=2');
+  // const res = await fetch('http://www.json-generator.com/api/json/get/caUKNwsmdK?indent=2');
+  const res = await fetch('http://35.190.35.153/api/pages-to-create/all-sale');
+  const data = await res.json();
+
+  return (
+    {
+      props:
+        { data }
+    }
+  )
+
+}
