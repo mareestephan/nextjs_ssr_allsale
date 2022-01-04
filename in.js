d7938dd1df42b089ab30b72979a3bd2d01b330b2
Stephan Maree <35425202+mareestephan@users.noreply.github.com>
2022-01-04 16:03:53 +0200

    Update in.js

--- in.js
+++ in.js
@@ -1,6 +1,5 @@
-              <script type="text/javascript" >
               jQuery.getJSON('/cart.js', function(cart) {
             // now have access to Shopify cart object
-                 alert('There are now ' + cart.item_count + ' items in the cart.'  + 'The total is ' + cart.total_price + cart  )
-              } );
-              </script>
+            document.getElementsByClassName("test-cart-credit-form").element.innerHTML = cart.total_price
+                //  alert('There are now ' + cart.item_count + ' items in the cart.'  + 'The total is ' + cart.total_price + cart  )
+              } ;
