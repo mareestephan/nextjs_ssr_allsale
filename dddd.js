606025bd003fb6f507ce23facfc51ea9291ce18d
Stephan Maree <35425202+mareestephan@users.noreply.github.com>
2022-01-04 16:43:43 +0200

    Add files via upload

---
+++ dddd.js
@@ -0,0 +1,7 @@
+      jQuery.getJSON('https://https-fullhouse-co-za.myshopify.com/cart.js', function(cart) {
+            // now have access to Shopify cart object
+            document.getElementById("test-cart-credit-form").innerHTML = 'cart.total_price'
+            document.getElementsByClassName("test-cart-credit-form").innerHTML = 'cart.total_price22'
+                //  alert('There are now ' + cart.item_count + ' items in the cart.'  + 'The total is ' + cart.total_price + cart  )
+              } );
+              
