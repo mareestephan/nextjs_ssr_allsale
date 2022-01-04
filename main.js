b52abb2db417fc98448cfb2ded656ad8a1d1e6b6
Stephan Maree <35425202+mareestephan@users.noreply.github.com>
2022-01-04 16:18:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+      jQuery.getJSON('/cart.js', function(cart) {
+            // now have access to Shopify cart object
+            document.getElementsByClassName(".test-cart-credit-form").innerHTML = cart.total_price
+                //  alert('There are now ' + cart.item_count + ' items in the cart.'  + 'The total is ' + cart.total_price + cart  )
+              } );
+              
