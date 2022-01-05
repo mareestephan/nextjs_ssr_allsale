97305eac33ce77e9388347d1f88851ceea7826d8
Stephan Maree <35425202+mareestephan@users.noreply.github.com>
2022-01-05 10:24:33 +0200

    Delete asdasd.js

--- asdasd.js
+++
@@ -1,7 +0,0 @@
-      jQuery.getJSON('/cart.js', function(cart) {
-            // now have access to Shopify cart object
-            document.getElementById("test-cart-credit-form").innerHTML = 'cart.total_price'
-            document.getElementsByClassName("test-cart-credit-form").innerHTML = 'cart.total_price22'
-                //  alert('There are now ' + cart.item_count + ' items in the cart.'  + 'The total is ' + cart.total_price + cart  )
-              } );
-              
